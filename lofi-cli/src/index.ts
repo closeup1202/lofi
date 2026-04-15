7edd7b74891d5cd2e396b69feb7b049ab2b1da41
closeup1202 <closeup1202@gmail.com>
2026-04-15 14:31:43 +0900

    docs(cli): change cli main description(2)

--- lofi-cli/src/index.ts
+++ lofi-cli/src/index.ts
@@ -11,7 +11,7 @@ const program = new Command()
 program
     .name('lofi')
     .description('Method-level latency regressions between deploys')
-    .version('0.2.4')
+    .version('0.2.5')
 
 function handleError(err: unknown): never {
     if (err instanceof LofiConnectionError) {
